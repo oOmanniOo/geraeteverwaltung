cc2fe768bbdb10a19dfc1d2863e57ff950da37c8
oOmanniOo <poker_face@gmx.net>
2025-01-18 21:44:35 +0100

    Geräte bearbeiten und anlegen hinzugefügt

---
+++ geraete/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Geraet
+
+class GeraeteForm(forms.ModelForm):
+    class Meta:
+        model = Geraet
+        fields = ['bezeichnung', 'seriennummer', 'identifikation', 'status', 'hersteller', 'kaufdatum']
