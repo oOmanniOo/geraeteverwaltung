82cced766af3c38f8e78f054a2d5ec84571a3a9b
oOmanniOo <poker_face@gmx.net>
2025-01-18 21:36:23 +0100

    Geräte anlegen und bearbeiten hinzuigefügt

--- geraete/urls.py
+++ geraete/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.geraete_liste, name='geraete_liste'),
     path('<int:id>/', views.geraete_detail, name='geraete_detail'),
+    path('create/', views.geraete_create, name='geraete_create'),
+    path('<int:id>/edit/', views.geraete_edit, name='geraete_edit'),
 ]
--- geraete/views.py
+++ geraete/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Geraet
+from .forms import GeraeteForm
 
 def geraete_liste(request):
     geraete = Geraet.objects.all()
@@ -8,3 +9,33 @@ def geraete_liste(request):
 def geraete_detail(request, id):
     geraet = get_object_or_404(Geraet, id=id)
     return render(request, 'geraete/geraete_detail.html', {'geraet': geraet})
+
+def geraete_create(request):
+    if request.method == 'POST':
+        form = GeraeteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('geraete_liste')
+    
+    else:
+        form = GeraeteForm()
+        
+    return render(request, 'geraete/geraete_create.html', {'form': form})
+
+def geraete_edit(request, id):
+    geraet = get_object_or_404(Geraet, id=id)
+
+    if request.method == 'POST':
+        form = GeraeteForm(request.POST, instance=geraet)
+
+        if form.is_valid():
+            identifikation = form.cleaned_data.get('identifikation')
+            if Geraet.objects.exclude(id=geraet.id).filter(identifikation=identifikation).exists():
+                form.add_error('identifikation', 'Dieses Gerät mit dieser Identifikation existiert bereits.')
+            else:
+                form.save()
+                return redirect('geraete_liste')
+    else:
+        form = GeraeteForm(instance=geraet)
+
+    return render(request, 'geraete/geraete_edit.html', {'form': form})
