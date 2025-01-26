5875a71f24d42b1425b984c48280e1ff20d724d0
oOmanniOo <poker_face@gmx.net>
2025-01-26 17:05:39 +0100

    crispy_forms hinzugefügt

--- geraeteverwaltung/settings.py
+++ geraeteverwaltung/settings.py
@@ -41,9 +41,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'geraete',
     'pruefung',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
