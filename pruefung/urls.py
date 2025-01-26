d4f989f14109df2a564a7f88c281f033f26d0716
oOmanniOo <poker_face@gmx.net>
2025-01-26 16:49:22 +0100

    Pruefungs Details und Prüfung durchführen hinzugefügtt

--- pruefung/urls.py
+++ pruefung/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'pruefung'
 
 urlpatterns = [
-    path('', views.pruefung_liste, name='pruefungs_liste')    
+    path('', views.pruefung_liste, name='pruefungs_liste'),
+    path('<int:id>/', views.pruefung_detail, name='pruefung_detail'),
+    path('pruefen/', views.pruefung_starten, name='pruefung_durchfueren'),
 ]
 
--- pruefung/views.py
+++ pruefung/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Pruefung
+from .forms import PruefungForm
 
 # Create your views here.
 def pruefung_liste(request):
     pruefungen = Pruefung.objects.all()
     return render(request, 'pruefung/pruefung_liste.html', {'pruefungen' : pruefungen})
+
+def pruefung_detail(request, id):
+    pruefung = get_object_or_404(Pruefung, id=id)
+    return render(request, 'pruefung/pruefung_detail.html', {'pruefung': pruefung})
+
+def pruefung_starten(request):
+    if request.method == 'POST':
+        form = PruefungForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('pruefung:pruefungs_liste')
+    
+    else:
+        form = PruefungForm()
+    
+    return render(request, 'pruefung/pruefung_durchfuehren.html', {'form': form})
