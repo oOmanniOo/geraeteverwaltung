e7f99732b335c7cafcce9703a6bfd547fb6e27ef
oOmanniOo <poker_face@gmx.net>
2025-03-15 23:03:02 +0100

    Fahrzeug Check hinzugefügt

--- pruefung/admin.py
+++ pruefung/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Befund, Pruefung, Art, Checkliste_Fragen, Checkliste_Ergebnis, Naechste_Pruefung
+from .models import Befund, Pruefung, Art, Checkliste_Fragen, Checkliste_Ergebnis, Naechste_Pruefung, Fahrzeug_Pruefung, Fahrzeug_Checkliste_Ergebnis, Fahrzeug_Fragen, Fahrzeug_Vorlage, Monat
 
 # Register your models here.
 admin.site.register(Befund)
@@ -8,3 +8,8 @@ admin.site.register(Art)
 admin.site.register(Checkliste_Ergebnis)
 admin.site.register(Checkliste_Fragen)
 admin.site.register(Naechste_Pruefung)
+admin.site.register(Fahrzeug_Fragen)
+admin.site.register(Fahrzeug_Vorlage)
+admin.site.register(Fahrzeug_Pruefung)
+admin.site.register(Fahrzeug_Checkliste_Ergebnis)
+admin.site.register(Monat)
